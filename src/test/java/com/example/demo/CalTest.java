8404bc1a7cd89726844acf2c0725bc94b610de7c
Anirudh bk <anirudh.bk@sonata-software.com>
2022-08-25 14:11:06 +0530

    update

--- src/test/java/com/example/demo/CalTest.java
+++ src/test/java/com/example/demo/CalTest.java
@@ -5,9 +5,6 @@ package com.example.demo;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.Test;
-import org.junit.jupiter.api.Assertions;
-
-import com.example.demo.Calculator;
 
 
 
@@ -16,8 +13,7 @@ public class CalTest {
 	@Test
 	public void addTest1() {
 		Calculator c1 = new Calculator();
-		int x = c1.div(1,1);
-		   assertThrows(ArithmeticException.class,()->c1.div(1,0),"it is an error");
+		assertThrows(ArithmeticException.class,()->c1.div(1,0),"it is an error");
 		   
 		
 	}
